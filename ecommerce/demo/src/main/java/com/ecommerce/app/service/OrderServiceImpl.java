0753cd8e00c510aafc9006706386af76eb43caa8
Katacoda Scenario <scenario@katacoda.com>
2026-05-28 08:32:56 +0000

    Implemented OrderService class

--- ecommerce/demo/src/main/java/com/ecommerce/app/service/OrderServiceImpl.java
+++ ecommerce/demo/src/main/java/com/ecommerce/app/service/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ecommerce.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.app.dto.request.OrderRequest;
 import com.ecommerce.app.dto.response.OrderResponse;
+import com.ecommerce.app.entity.Order;
 import com.ecommerce.app.exception.ResourceNotFoundException;
 import com.ecommerce.app.mapper.OrderMapper;
 import com.ecommerce.app.repository.OrderRepository;
@@ -16,11 +18,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
-    private final OrderRepository orderRepo;
+    private final OrderRepository orderRepository;
     @Override
     public List<OrderResponse> getAll() {
         // TODO Auto-generated method stub
-        return orderRepo.findAll()
+        return orderRepository.findAll()
                     .stream()
                     .map(OrderMapper::mapToOrderResponse)
                     .toList();
@@ -29,25 +31,31 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public OrderResponse getById(Long id) {
         // TODO Auto-generated method stub
-        return OrderMapper.mapToOrderResponse(orderRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("No orders found with id "+id)));
+        return OrderMapper.mapToOrderResponse(orderRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No orders found with id "+id)));
     }
 
     @Override
-    public OrderResponse create(OrderRequest request) {
+    public OrderResponse create(OrderRequest orderRequest) {
         // TODO Auto-generated method stub
-        
+        Order order = OrderMapper.mapToOrder(orderRequest);
+        return OrderMapper.mapToOrderResponse(orderRepository.save(order));
     }
 
     @Override
-    public OrderResponse update(Long id, OrderRequest request) {
+    public OrderResponse update(Long id, OrderRequest orderRequest) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        Optional<Order> existingOrder = orderRepository.findById(id);
+        if (existingOrder.isEmpty())
+            throw new ResourceNotFoundException(String.format("Resource not found with id: %s",id));
+        Order orderToUpdate = OrderMapper.mapToOrder(orderRequest);
+        Order savedOrder = orderRepository.save(orderToUpdate);
+        return OrderMapper.mapToOrderResponse(savedOrder);
     }
 
     @Override
     public void delete(Long id) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        orderRepository.deleteById(id);
     }
     
 }
