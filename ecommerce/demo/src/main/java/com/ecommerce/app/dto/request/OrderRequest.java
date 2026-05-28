8e461d0cef3c958ced1793ac470197ff0a17a3b0
Katacoda Scenario <scenario@katacoda.com>
2026-05-28 10:49:10 +0000

    Validated OrderRequest fields.

--- ecommerce/demo/src/main/java/com/ecommerce/app/dto/request/OrderRequest.java
+++ ecommerce/demo/src/main/java/com/ecommerce/app/dto/request/OrderRequest.java
@@ -1,7 +1,13 @@
 package com.ecommerce.app.dto.request;
 
 public record OrderRequest(
+    @NotNull(message = "ID cannot be null")
     Long userId,
+
+    @NotNull(message = "Total Amount cannot be null")
+    @DecimalMin(value = 0, message = "Total Amount must be greater than or equal to zero")
     Double totalAmount,
+
+    @NotBlank(message = "Status cannot be null")
     String status
 ) {}
